643c39a9c34614629bec2c7458a5f3d183038c3a
Archibal228 <ivany4.081995@gmail.com>
2016-09-06 22:30:30 +0300

    no dick

--- src/CharAmount.java
+++ src/CharAmount.java
@@ -1,7 +1,14 @@
 
 public class CharAmount {
-	public static void main (String[] args){
-		Str
+	public static void main(String[] args) {
+		String st1, st2;
+		String st = "Я люблю Jav";
+		int kol = st.length();
+		System.out.println(st);
+		System.out.println(kol);
+		st1 = st.substring(0, kol / 2);
+		st2 = st.substring(kol / 2);
+		System.out.println(st1);
+		System.out.println(st2);
 	}
-
 }
