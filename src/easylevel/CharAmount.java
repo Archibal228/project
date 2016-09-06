ecb91900a2dc03a22e1e99a1d9d0d3b73bb94ce6
Archibal228 <ivany4.081995@gmail.com>
2016-09-06 22:49:55 +0300

    square_table

--- src/easylevel/CharAmount.java
+++ src/easylevel/CharAmount.java
@@ -1,3 +1,4 @@
+package easylevel;
 
 public class CharAmount {
 	public static void main(String[] args) {
---
+++ src/easylevel/SquareTable.java
@@ -0,0 +1,10 @@
+package easylevel;
+
+public class SquareTable {
+public static void main(String[] args){
+	for(int i = 0; i<11; i++){
+		System.out.print(i+" ");
+		System.out.println(i*i);
+	}
+}
+}
